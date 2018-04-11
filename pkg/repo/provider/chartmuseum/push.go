3072299a9919f6cf3cdaad8e17dc1c8816f7a686
Josh Dolitsky <jdolitsky@gmail.com>
2018-04-10 19:59:35 -0500

    sample oauth flow

--- pkg/repo/provider/chartmuseum/push.go
+++ pkg/repo/provider/chartmuseum/push.go
@@ -23,15 +23,14 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"k8s.io/helm/pkg/chartutil"
+	"k8s.io/helm/pkg/version"
 	"mime/multipart"
 	"net/http"
 	"net/url"
 	"os"
 	"path"
 	"strings"
-
-	"k8s.io/helm/pkg/chartutil"
-	"k8s.io/helm/pkg/version"
 )
 
 // Push uploads a chart package to a ChartMuseum web server.
@@ -48,10 +47,16 @@ func (cm *ChartMuseum) Push(packageAbsPath string, namespace string) error {
 	}
 	fmt.Println(msg + "...")
 
-	return uploadPackage(packageAbsPath, cm.Config.URL, namespace, cm.Config.Username, cm.Config.Password)
+	return uploadPackage(packageAbsPath, cm.Config.URL, namespace, cm.Config.Username, cm.Config.Password, "")
 }
 
-func uploadPackage(packageAbsPath string, endpoint string, namespace string, username string, password string) error {
+func uploadPackage(packageAbsPath string, endpoint string, namespace string, username string, password string, token string) error {
+	if token == "" {
+		fmt.Println("[1] Attempt operation...")
+	} else {
+		fmt.Println("[5] Retry operation with access token...")
+	}
+
 	client := &http.Client{}
 
 	u, err := url.Parse(endpoint)
@@ -68,8 +73,11 @@ func uploadPackage(packageAbsPath string, endpoint string, namespace string, use
 
 	req.Header.Set("User-Agent", "Helm/"+strings.TrimPrefix(version.GetVersion(), "v"))
 
-	if username != "" && password != "" {
-		req.SetBasicAuth(username, password)
+	//if username != "" && password != "" {
+	//	req.SetBasicAuth(username, password)
+	//}
+	if token != "" {
+		req.Header.Set("Authorization", "Bearer "+token)
 	}
 
 	resp, err := client.Do(req)
@@ -77,7 +85,91 @@ func uploadPackage(packageAbsPath string, endpoint string, namespace string, use
 		return err
 	}
 
-	if resp.StatusCode != 201 {
+	if token == "" && resp.StatusCode == 401 {
+		fmt.Println("[2] Received 401 from chartmuseum with auth instructions...")
+
+		wwwAuth := resp.Header.Get("WWW-Authenticate")
+		if wwwAuth == "" {
+			return errors.New("missing WWW-Authenticate header")
+		}
+
+		parts := strings.Split(resp.Header.Get("WWW-Authenticate"), "Bearer ")
+		if len(parts) != 2 {
+			return errors.New("malformed WWW-Authenticate header")
+		}
+
+		keys := map[string]string{}
+		for _, v := range strings.Split(parts[1], "\",") {
+			tmp := strings.Split(v, "=")
+			if len(tmp) == 2 {
+				keys[tmp[0]] = strings.Trim(tmp[1], "\"")
+			}
+		}
+
+		realm, ok := keys["realm"]
+		if !ok {
+			return errors.New("No realm in WWW-Authenticate header")
+		}
+
+		service, ok := keys["service"]
+		if !ok {
+			return errors.New("No service in WWW-Authenticate header")
+		}
+
+		scope, ok := keys["scope"]
+		if !ok {
+			return errors.New("No scope in WWW-Authenticate header")
+		}
+
+		fmt.Println(fmt.Sprintf("[3] Requesting token from %s with scope %s...", realm, scope))
+		token = "xyz"
+
+		var jsonStr = []byte(fmt.Sprintf(`{
+			"client_id": "xxx",
+			"client_secret": "xxx",
+			"audience": "%s",
+			"grant_type": "password",
+			"username": "xxx",
+			"password": "xxx",
+			"scope": "%s"
+		}`, service, scope))
+
+		req, err = http.NewRequest("POST", realm, bytes.NewBuffer(jsonStr))
+		if err != nil {
+			return err
+		}
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("User-Agent", "Helm/"+strings.TrimPrefix(version.GetVersion(), "v"))
+
+		resp, err = client.Do(req)
+		if err != nil {
+			return err
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		if err != nil {
+			return err
+		}
+
+		type t struct {
+			AccessToken string `json:"access_token"`
+			ExpiresIn   int    `json:"expires_in"`
+			TokenType   string `json:"token_type"`
+		}
+		var tt t
+		err = json.Unmarshal(b, &tt)
+		if err != nil {
+			return err
+		}
+		token = tt.AccessToken
+
+		fmt.Println("[4] Received access token from auth server")
+
+		return uploadPackage(packageAbsPath, endpoint, namespace, username, password, token)
+
+	} else if resp.StatusCode != 201 {
 		b, err := ioutil.ReadAll(resp.Body)
 		defer resp.Body.Close()
 		if err != nil {
@@ -92,6 +184,7 @@ func uploadPackage(packageAbsPath string, endpoint string, namespace string, use
 		return errors.New(fmt.Sprintf("%d: %s", resp.StatusCode, er.Error))
 	}
 
+	fmt.Println("[6] Client authorized, operation successful")
 	fmt.Println("Done.")
 	return nil
 }
