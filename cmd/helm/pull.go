8af714d36ad0f2a33830bca9904bbfaf92792afc
Josh Dolitsky <jdolitsky@gmail.com>
2019-01-02 09:47:13 -0600

    remove old pull code
    Signed-off-by: Josh Dolitsky <jdolitsky@gmail.com>

--- cmd/helm/pull.go
+++ cmd/helm/pull.go
@@ -17,18 +17,9 @@ limitations under the License.
 package main
 
 import (
-	"context"
-	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
-	"os"
-	"strings"
 
-	"github.com/containerd/containerd/content/local"
-	"github.com/containerd/containerd/images"
-	"github.com/containerd/containerd/remotes"
-	"github.com/containerd/containerd/remotes/docker"
 	"github.com/spf13/cobra"
 
 	"k8s.io/helm/cmd/helm/require"
@@ -98,71 +89,7 @@ func newPullCmd(out io.Writer) *cobra.Command {
 	return cmd
 }
 
-// adapted from https://gist.github.com/cpuguy83/541dc445fad44193068a1f8f365a9c0e
 func (o *pullOptions) run(out io.Writer) error {
-	parts := strings.Split(o.chartRef, ":")
-	if len(parts) != 2 {
-		return errors.New("invalid chart format, must be NAME[:TAG|@DIGEST]")
-	}
-	chartName := parts[0]
-	chartVersion := parts[1]
-
-	fmt.Printf("%s: Pulling from %s\n", chartVersion, chartName)
-
-	cacheDir := settings.Home.Cache()
-	os.MkdirAll(cacheDir, os.ModePerm)
-
-	tmpdir, err := ioutil.TempDir(cacheDir, ".helm-pull")
-	if err != nil {
-		return err
-	}
-	defer func() {
-		e := recover()
-		os.RemoveAll(tmpdir)
-		if e == nil {
-			return
-		}
-		panic(e)
-	}()
-
-	// this store is a local content addressible store
-	// it satifies the "Ingestor" interface used by the call to `images.Dispatch`
-	cs, err := local.NewStore(cacheDir)
-	if err != nil {
-		return err
-	}
-
-	resolver := docker.NewResolver(docker.ResolverOptions{})
-	ctx := context.Background()
-
-	name, desc, err := resolver.Resolve(ctx, o.chartRef)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Digest: %s\n", desc.Digest)
-	fmt.Printf("MediaType: %s\n", desc.MediaType)
-
-	fetcher, err := resolver.Fetcher(ctx, name)
-	if err != nil {
-		return err
-	}
-
-	r, err := fetcher.Fetch(ctx, desc)
-	if err != nil {
-		return err
-	}
-	defer r.Close()
-
-	// Handler which reads a descriptor and fetches the referenced data (e.g. image layers) from the remote, recursively
-	h := images.Handlers(remotes.FetchHandler(cs, fetcher), images.ChildrenHandler(cs))
-
-	// This traverses the OCI descriptor to fetch the image and store it into the local store initialized above.
-	// All content hashes are verified in this step
-	if err := images.Dispatch(ctx, h, desc); err != nil {
-		return err
-	}
-
-	fmt.Printf("Status: Chart is up to date for %s\n", o.chartRef)
+	fmt.Printf("Pulling %s\n", o.chartRef)
 	return nil
 }
