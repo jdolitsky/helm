848f029f6cb42f36d1b7a78c51d61626da6bfa67
Josh Dolitsky <jdolitsky@gmail.com>
2019-01-31 03:09:10 -0600

    pull in dependencies for registry tests
    Signed-off-by: Josh Dolitsky <jdolitsky@gmail.com>

--- pkg/registry/client_test.go
+++ pkg/registry/client_test.go
@@ -17,11 +17,70 @@ limitations under the License.
 package registry
 
 import (
+	"context"
+	"fmt"
+	"net"
 	"testing"
+	"time"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/containerd/containerd/remotes"
+	"github.com/containerd/containerd/remotes/docker"
+	"github.com/docker/distribution/configuration"
+	"github.com/docker/distribution/registry"
+	_ "github.com/docker/distribution/registry/storage/driver/inmemory"
+
+	//ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	//orascontent "github.com/deislabs/oras/pkg/content"
+
+	"github.com/stretchr/testify/suite"
 )
 
-func TestClient(t *testing.T) {
-	assert.Equal(t, true, true)
+type RegistryClientTestSuite struct {
+	suite.Suite
+	DockerRegistryHost string
+	RegistryClient     *Client
+}
+
+func (suite *RegistryClientTestSuite) SetupSuite() {
+	config := &configuration.Configuration{}
+	port, err := getFreePort()
+	if err != nil {
+		suite.Nil(err, "no error finding free port for test registry")
+	}
+	suite.DockerRegistryHost = fmt.Sprintf("localhost:%d", port)
+	config.HTTP.Addr = fmt.Sprintf(":%d", port)
+	config.HTTP.DrainTimeout = time.Duration(10) * time.Second
+	config.Storage = map[string]configuration.Parameters{"inmemory": map[string]interface{}{}}
+	dockerRegistry, err := registry.NewRegistry(context.Background(), config)
+	suite.Nil(err, "no error creating test registry")
+
+	// Start Docker registry
+	go dockerRegistry.ListenAndServe()
+}
+
+func TestRegistryClientTestSuite(t *testing.T) {
+	suite.Run(t, new(RegistryClientTestSuite))
+}
+
+func newContext() context.Context {
+	return context.Background()
+}
+
+func newResolver() remotes.Resolver {
+	return docker.NewResolver(docker.ResolverOptions{})
+}
+
+// borrowed from https://github.com/phayes/freeport
+func getFreePort() (int, error) {
+	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+	if err != nil {
+		return 0, err
+	}
+
+	l, err := net.ListenTCP("tcp", addr)
+	if err != nil {
+		return 0, err
+	}
+	defer l.Close()
+	return l.Addr().(*net.TCPAddr).Port, nil
 }
